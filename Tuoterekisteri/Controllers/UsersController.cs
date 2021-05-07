89cddd11a5bd4ebe4092f3c13516f2c2c47f201d
Vickechino <60480110+Vickechino@users.noreply.github.com>
2021-05-07 13:18:22 +0300

    Sisäänkirjautuminen ja käyttäjienhallinta lisätty

---
+++ Tuoterekisteri/Controllers/UsersController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Tuoterekisteri.Models;
+
+namespace Tuoterekisteri.Controllers
+{
+    public class UsersController : Controller
+    {
+        LaitehallintaEntities db = new LaitehallintaEntities();
+        // GET: Users
+        public ActionResult Index()
+        {
+            if (Session["Permission"] != null && Session["Permission"].ToString() == "1")
+            {
+                List<User> model = db.Users.ToList();
+                db.Dispose();
+                return View(model);
+            }
+            else return RedirectToAction("login", "Users");
+        }
+        public ActionResult Login()
+        {
+            if (Session["userName"] != null) { return RedirectToAction("Index", "Home"); }
+            else return View();
+        }
+        [HttpPost]
+        public ActionResult Authorize(User LoginModel)
+        {
+            var LoggedUser = db.Users.SingleOrDefault(x => x.userName == LoginModel.userName && x.password == LoginModel.password);
+            if (LoggedUser != null)
+            {
+                ViewBag.LoginMessage = "Successful login";
+                ViewBag.Loggedstatus = "In";
+                Session["UserName"] = LoggedUser.userName;
+                Session["Permission"] = LoggedUser.admin;
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.LoginMessage = "Login unsuccessfull";
+                ViewBag.Loggedstatus = "Out";
+                //LoginModel.LoginErrorMessage = "Väärä käyttäjätunnus/salasana";
+                return View("Login", LoginModel);
+            }
+        }
+        public ActionResult LogOut()
+        {
+            Session.Abandon();
+            ViewBag.LoggedStatus = "Out";
+            return RedirectToAction("Index", "Home");
+        }
+        public ActionResult Create()
+        {
+            if (Session["UserName"] != null)
+            {
+                return View();
+            }
+            else return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "userName, password, email, firstName, lastName, admin")] User newUser)
+        {
+            if (ModelState.IsValid && Session["UserName"] != null)
+            {
+                try
+                {
+                    db.Users.Add(newUser);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                catch
+                {
+                    ViewBag.CreateUserError = "Error! Username taken?";
+                    return View();
+                }
+
+            }
+            return View(User);
+        }
+        public ActionResult Delete(int? id)
+        {
+            if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            User user = db.Users.Find(id);
+            if (user == null) return RedirectToAction("Index", "Home");
+            if (Session["UserName"] != null)
+            {
+                return View(user);
+            }
+            else return RedirectToAction("Index");
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            if (Session["UserName"] != null && Session["Permission"].ToString() == "1")
+            {
+                try
+                {
+                    User user = db.Users.Find(id);
+
+                    db.Users.Remove(user);
+                    db.SaveChanges();
+                    if (Session["UserName"].ToString().ToLower() == user.userName.ToString().ToLower()) { return RedirectToAction("Logout", "Users"); } //Should consider removing some casts
+                    return RedirectToAction("Index");
+                }
+                catch
+                {
+                    ViewBag.DeleteUserError = "Error deleting user. User does no-longer exist?";
+                    return View();
+                }
+
+            }
+            else return RedirectToAction("Index");
+        }
+    }
+}
