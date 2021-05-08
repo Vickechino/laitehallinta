0e6a42da391039075ba1bb8df48a938c2968de68
zzzcv <zzzcv@DESKTOP-QCQD1HJ>
2021-05-08 21:32:01 +0300

    Language

---
+++ Tuoterekisteri/Lang.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tuoterekisteri
+{
+    public class Lang
+    {
+        public static int lang = 1; // Setting initial language
+    }
+}
+
+// 0 = ENG
+// 1 = FIN
+// 2 = SWE
+
