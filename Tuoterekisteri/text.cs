9a6a39d25f25b83c75513a2ffa9a968041cce657
Vickechino <60480110+Vickechino@users.noreply.github.com>
2021-05-11 12:20:06 +0300

    language

--- Tuoterekisteri/text.cs
+++ Tuoterekisteri/text.cs
@@ -12,6 +12,13 @@ namespace Tuoterekisteri
 	{"Menu", "Valikko", "Meny" }, // 0
 	{"Login", "Kirjaudu", "Logga in" }, // 1
 	{"Username", "Käyttäjänimi", "Användarnamn" }, // 2
+	{"Password", "Salasana", "Lösenord" }, // 3
+	{"Firstname", "Etunimi", "Förnamn" }, // 4
+	{"Lastname", "Sukunimi", "Efternamn" }, // 5
+	{"Email", "Sähköposti", "e-postaddress" }, // 6
+	{"Log out", "Kirjaudu ulos", "Logga ut" }, // 7
+	{"Create", "Luo", "Skapa" }, // 8
+	{"User Management", "Käyttäjienhallinta", "Användarhantering" }, // 9
 
 
 
