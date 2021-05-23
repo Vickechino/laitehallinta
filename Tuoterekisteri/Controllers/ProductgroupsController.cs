63b0dfd19494467ba38ac6df39ec45441cb06b05
zzzcv <zzzcv@DESKTOP-QCQD1HJ>
2021-05-23 16:06:43 +0300

    Commit...

---
+++ Tuoterekisteri/Controllers/ProductgroupsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Tuoterekisteri.Models;
+
+namespace Tuoterekisteri.Controllers
+{
+    public class ProductgroupsController : Controller
+    {
+        private LaitehallintaEntities db = new LaitehallintaEntities();
+
+        // GET: Productgroups
+        public ActionResult Index()
+        {
+            return View(db.Productgroups.ToList());
+        }
+
+        // GET: Productgroups/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Productgroup productgroup = db.Productgroups.Find(id);
+            if (productgroup == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productgroup);
+        }
+
+        // GET: Productgroups/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Productgroups/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "product_group_id,product_group_name")] Productgroup productgroup)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Productgroups.Add(productgroup);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(productgroup);
+        }
+
+        // GET: Productgroups/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Productgroup productgroup = db.Productgroups.Find(id);
+            if (productgroup == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productgroup);
+        }
+
+        // POST: Productgroups/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "product_group_id,product_group_name")] Productgroup productgroup)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(productgroup).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(productgroup);
+        }
+
+        // GET: Productgroups/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Productgroup productgroup = db.Productgroups.Find(id);
+            if (productgroup == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productgroup);
+        }
+
+        // POST: Productgroups/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Productgroup productgroup = db.Productgroups.Find(id);
+            db.Productgroups.Remove(productgroup);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- Tuoterekisteri/text.cs
+++ Tuoterekisteri/text.cs
@@ -10,6 +10,8 @@ public class T
 public static string[,] txt =
 {
 
+// T.txt[0, L.nr]
+
 {"ENG", "FIN", "SWE" }, // 0
 {"Login", "Kirjaudu", "Logga in" }, // 1
 {"Username", "Käyttäjänimi", "Användarnamn" }, // 2
@@ -22,6 +24,28 @@ public static string[,] txt =
 {"Create", "Lisää", "Lägg till"}, // 9
 {"Delete", "Poista", "Radera"}, // 10
 {"Edit", "Muuta", "Ändra"}, // 11
+{"Users", "Käyttäjät", "Användare"}, //12
+{"Locations", "Paikat", "Platser"}, // 13
+{"Location", "Paikka", "Plats"}, // 14
+{"Back to List", "Takaisin listaan", "Tillbaka till listan"}, //15
+{"Index", "Indeksi", "Index"}, // 16
+{"Create New", "Luo uusi", "Lägg till ny"}, // 17
+{"Details", "Tiedot", "Detaljer"}, // 18
+{"Are you sure you want to delete this?",
+"Oletko varma että haluat poistaa tämän?",
+"Är du säker att du vill radera detta"},  //19
+{"Product Groups", "Tuoteryhmät", "Produktgrupper"}, //20
+{"", "", " "}, // 21
+{" ", " ", " "}, // 22
+{" ", " ", " "}, // 23
+{" ", " ", " "}, // 24
+{" ", " ", " "}, // 25
+{" ", " ", " "}, // 26
+{" ", " ", " "}, // 27
+{" ", " ", " "}, // 28
+{" ", " ", " "}, // 29
+{" ", " ", " "}, // 30
+{" ", " ", " "}, // 31
 
 
 };
